49788f4c15adf55ff5d25d8efa45cde25fdb8e3e
Roch Armengaud <armengaud.roch@live.fr>
2022-05-10 15:07:37 +0200

    Closed the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,29 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+// On sélectionne l'élement avec la classe .close
+const closeBtn = document.querySelector('.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+
+// close modal form event
+// Lorsque l'utilisateur clique sur le bouton "fermer", la modale se ferme
+closeBtn.addEventListener('click', (closeModal) => {
+  modalbg.style.display = 'none';
+});
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
-
-
