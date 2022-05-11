ff96dbea4ac19457f0e0d3f90c561b1776adf84c
Roch Armengaud <armengaud.roch@live.fr>
2022-05-11 16:12:36 +0200

    Modified the closing modal code & added for const

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+// Fonctions
+
 function editNav() {
   var x = document.getElementById('myTopnav');
   if (x.className === 'topnav') {
@@ -10,20 +12,39 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
+const modalClose = document.querySelector('.close');
 const formData = document.querySelectorAll('.formData');
-// On sélectionne l'élement avec la classe .close
-const closeBtn = document.querySelector('.close');
 
+// Le formulaire doit être valide quand l'utilisateur envoie les données
+const firstName = document.querySelector('first');
+const lastName = document.querySelector('name');
+const eMail = document.querySelector('email');
+const birthDate = document.querySelector('birthdate');
+const tournamentQuantity = document.querySelector('quantity');
+const loc1 = document.querySelector('location1');
+const loc2 = document.querySelector('location2');
+const loc3 = document.querySelector('location3');
+const loc4 = document.querySelector('location4');
+const loc5 = document.querySelector('location5');
+const loc6 = document.querySelector('location6');
+
+// RegExp pour le format de la date et des nombres autorisés
+const dateFormat = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
+const numbers = /^[0-9]+$/;
+
+// MODALE
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
-
-// close modal form event
-// Lorsque l'utilisateur clique sur le bouton ".close", la modale se ferme
-closeBtn.addEventListener('click', (closeModal) => {
-  modalbg.style.display = 'none';
-});
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = 'block';
+}
+
+// close modal event
+modalClose.addEventListener('click', closeModal);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = 'none';
 }
