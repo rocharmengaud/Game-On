5a7773072b9692fded47b188927a5a0b72115c00
Roch Armengaud <armengaud.roch@live.fr>
2022-05-13 17:23:14 +0200

    Resolved issue #2

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,59 +1,96 @@
-// functions to validate inputs
-function validate() {
-  if (
-    firstValidation() === true &&
-    emailValidation() === true &&
-    quantityValidation() === true &&
-    locationValidation() === true
-  ) {
+// On crée une variable contenant une fonction qui va vérifier tous les champs du formulaire
+const validateInputs = () => {
+  validateFirst();
+  validateLast();
+  validateEmail();
+  validateQuantity();
+  validateLocation();
+  validateConditions();
+};
+
+// on crée la variable qui va contenir le formulaire complet
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  // on fait en sorte que la page ne se refresh pas lorsque l'on clique sur submit
+  e.preventDefault();
+  validateInputs();
+});
+
+// Checks the first name
+function validateFirst() {
+  const first = document.querySelector('#first');
+  // console.log(first.value);
+  // .value correspond à une propriété definie d'un élément en JS
+  const firstLength = first.value.length;
+  if (firstLength < 2 || firstLength === '') {
+    document.querySelector('.first-error').innerHTML =
+      'Veuillez entrer un prénom valide';
+  } else {
+    document.querySelector('.first-error').innerHTML = '';
+  }
+}
+
+// Checks the last name
+function validateLast() {
+  const last = document.querySelector('#last');
+  const lastLength = last.value.length;
+  if (lastLength < 2 || lastLength === '') {
+    document.querySelector('.last-error').innerHTML =
+      'Veuillez entrer un nom valide';
+  } else {
+    document.querySelector('.last-error').innerHTML = '';
+  }
+}
+
+// Checks if the user has typed a valid e-mail format
+function validateEmail() {
+  const email = document.querySelector('#email');
+  let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  if (regex.test(email.value) === false) {
+    document.querySelector('.email-error').innerHTML =
+      'Veuillez entrer une adresse mail valide';
+  } else {
+    document.querySelector('.email-error').innerHTML = '';
+  }
+}
+
+// Checks if the user has selected a quantity
+function validateQuantity() {
+  const quantity = document.querySelector('#quantity');
+  let regex = /^[0-9]{1,2}$/;
+  if (regex.test(quantity.value) === false) {
+    document.querySelector('.quantity-error').innerHTML =
+      'Veuillez sélectionner un chiffre ou un nombre';
+  } else {
+    document.querySelector('.quantity-error').innerHTML = '';
+  }
+}
+
+// on crée la variable qui va contenir tous les boutons radio
+const radios = document.querySelectorAll('.checkbox-input[type=radio]');
+
+// Checks if the user has selected a location
+function validateLocation() {
+  for (let radio of radios) {
+    if (radio.checked === true) {
+      document.querySelector('.location-error').innerHTML = '';
+      return true;
+    }
+  }
+  document.querySelector('.location-error').innerHTML =
+    'Veuillez sélectionner une ville';
+  return false;
+}
+
+// on crée la variable qui va contenir la case des conditions générales
+const conditionsGenerales = document.querySelector('#checkbox1');
+
+// Checks if the user has checked the cgu
+function validateConditions() {
+  if (conditionsGenerales.checked === true) {
+    console.log('ok');
     return true;
   }
   return false;
 }
-
-function firstValidation() {
-  let inputValue = document.getElementById('first').value;
-  if (inputValue !== null && inputValue.length > 2) return true;
-  else return false;
-}
-
-// ici on appelle la fonction firstValidation
-document.querySelector('#first').addEventListener('keydown', (event) => {
-  if (firstValidation() === false) {
-    document.querySelector('.first-error').innerHTML =
-      'Veuillez entrer un nom valide';
-  } else {
-    document.querySelector('.first-error').innerHTML = '';
-    // ici on effaçe le texte 'Veuillez entrer un nom valide'
-  }
-});
-
-function emailValidation() {
-  let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
-  let inputValue = document.getElementById('email').value;
-  return regex.test(inputValue);
-}
-
-document.querySelector('#email').addEventListener('keydown', (event) => {
-  if (emailValidation() === false) {
-    document.querySelector('.email-error').innerHTML =
-      'Veuillez entrer une adresse email valide';
-  } else {
-    document.querySelector('.email-error').innerHTML = '';
-  }
-});
-
-function quantityValidation() {
-  let regex = /^[0-9]+$/;
-  let inputValue = document.getElementById('quantity').value;
-  return regex.test(inputValue);
-}
-
-function locationValidation() {
-  let radioButtons = document.querySelectorAll('.checkbox-input[type=radio]');
-  for (let radio of radioButtons) {
-    if (radio.checked === true) return true;
-  }
-  return false;
-}
-// ce que la fonction va renvoyer par défaut (c'est pour ca qu'il n'y a pas de else)
